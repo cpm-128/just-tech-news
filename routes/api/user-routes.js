1e2ab22ad0bdd2fa03f2ad3a3bba56e1c35e494e
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-07 15:19:41 -0400

    added comments to be include with get single user data

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require('../../models');
+const { User, Post, Vote, Comment } = require('../../models');
 
 // ===============
 // CRUD OPERATIONS
@@ -41,6 +41,14 @@ router.get('/:id', (req, res) => {
                 attributes: ['title'],
                 through: Vote,
                 as: 'voted_posts'
+            },
+            { // comments
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
             }
         ]
     })
