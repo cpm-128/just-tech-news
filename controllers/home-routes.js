b3555ac7790d6c5a0528e5bb2d8e3f8d06c80870
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-11 14:00:19 -0400

    added home-routes to dynamically display post from database

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,20 +1,42 @@
 // user-facing routes, such as the homepage and login page
 
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    // which vews layout to render?
-    res.render('homepage', {
-        // pass in the post object
-        id: 1,
-        post_url: 'https://handlebarsjs.com/guide/',
-        title: 'Handlebars Docs',
-        created_at: new Date(),
-        vote_count: 10,
-        comments: [{}, {}],
-        user: {
-            username: 'test_user'
-        }
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // pass a single post object into the homepage template
+        //console.log('>> Post data ??' , dbPostData[0])
+        // plain: true is so you dont get a bunch of the other object data you're not needing
+        res.render('homepage', dbPostData[0].get({ plain: true }));
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
 });
 
