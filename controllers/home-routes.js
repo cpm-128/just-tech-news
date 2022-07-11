0239edb361f875a113566ee540c9c70a2dd3af5a
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-11 13:34:41 -0400

    added dynamic html in homepage.handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,19 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    // which vews layout to render?
+    res.render('homepage', {
+        // pass in the post object
+        id: 1,
+        post_url: 'https://handlebarsjs.com/guide/',
+        title: 'Handlebars Docs',
+        created_at: new Date(),
+        vote_count: 10,
+        comments: [{}, {}],
+        user: {
+            username: 'test_user'
+        }
+    });
 });
 
 module.exports = router;
