15dfd140a04df2d8daa1338e638b6143e5113b02
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-11 12:21:04 -0400

    installed handlebars, reorg directories for views and controllers, added basic homepage template

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+// make the frontend available to the client via middleware
+const path = require('path');
+
+// use Handlebars.js as app's template engine
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({})
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// use Handlebars.js as app's template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // middleware so express knows how to handle incoming data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// handle frontend public files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // turn on routes
+// routes must be after all static calls
 app.use(routes);
 
 // turn on connect to db and server
