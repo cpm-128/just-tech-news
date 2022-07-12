1f990334dedc116418408a95c8b6eebf1a703ef0
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-12 07:17:35 -0400

    add db routes to single post call

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -56,19 +56,50 @@ router.get('/login', (req, res) => {
 
 // GET a single post with comments and votes
 router.get('/post/:id', (req, res) => {
-    const post = {
-        id: 1,
-        post_url: 'https://handlebarsjs.com/guide/',
-        title: 'Handlebars Docs',
-        created_at: new Date(),
-        vote_count: 10,
-        comments: [ {} , {} ],
-        user: {
-            username: 'test_user'
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id', 'comment_text', 'post_id', 'user_id', 'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id.' });
+            return;
         }
-    };
-    // where to render the above data, what to package it and send it as to the render template
-    res.render('single-post', { post });
+
+        // serialize the data and assign variable to send to handlebars
+        const post = dbPostData.get({ plain: true });
+
+        // pass data to the template
+        res.render('single-post', { post });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
