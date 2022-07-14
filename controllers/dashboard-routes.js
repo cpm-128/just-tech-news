3c12984a2aa51e3db46ea730d6d128add92edc53
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-14 09:55:03 -0400

    dispay user published posts on dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
             'id',
             'post_url',
             'title',
-            'created at',
+            'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
