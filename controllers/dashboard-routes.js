9a39da6883231b62ffa6a3fb4f62a03da25a66b5
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-14 11:07:59 -0400

    auth user can edit and delete their posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -42,4 +42,50 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id', 'comment_text', 'post_id', 'user_id', 'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // serialize the data and assign variable to send to handlebars
+       if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        // pass data to the template
+        res.render('edit-post', {
+            post,
+            loggedIn: true
+        });
+        } else {
+            res.status(404).end();
+        };
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
